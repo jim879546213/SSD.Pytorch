4d737c9505e794c071b06e98558ff3313e99c862
jim879546213 <jim879546213@gmail.com>
2021-04-08 14:23:24 +0800

    Update voc0712.py

--- data/voc0712.py
+++ data/voc0712.py
@@ -38,9 +38,9 @@ class VOCAnnotationTransform(object):
     """
 
     def __init__(self, class_to_ind=None, keep_difficult=False):
-#         self.class_to_ind = class_to_ind or dict(
-#             zip(VOC_CLASSES, range(len(VOC_CLASSES))))
-        self.class_to_ind = {"TB":0}
+        self.class_to_ind = class_to_ind or dict(
+            zip(VOC_CLASSES, range(len(VOC_CLASSES))))
+#         self.class_to_ind = {"TB":0}
         self.keep_difficult = keep_difficult
 
     def __call__(self, target, width, height):
